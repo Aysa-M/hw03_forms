bad1478d02fb5f1943a0ff5abe34770236c4d74c
Aysa Matsakova <happiness390@gmail.com>
2021-12-29 14:46:20 +0300

    Upgrade

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -8,7 +8,6 @@ from .forms import PostForm
 from .models import Group, Post
 
 
-@login_required
 def index(request):
     """Information which is showing up on the start page."""
     post_list = Post.objects.all()
@@ -21,7 +20,6 @@ def index(request):
     return render(request, 'posts/index.html', context)
 
 
-@authorized_only
 def group_posts(request, slug):
     """Information for displaying on the page with posts grouped by GROUPS."""
     group = get_object_or_404(Group, slug=slug)
@@ -36,19 +34,18 @@ def group_posts(request, slug):
     return render(request, 'posts/group_list.html', context)
 
 
-@authorized_only
 def profile(request, username):
     """
     The view shows a page profile of an authorised user.
     """
-    user = get_object_or_404(User, username=username)
-    post_list_profile = Post.objects.filter(author=user).all()
-    paginator_profile = Paginator(post_list_profile, 10)
-    page_number_profile = request.GET.get('page')
-    page_obj = paginator_profile.get_page(page_number_profile)
-    post_quantity = post_list_profile.count()
+    author = get_object_or_404(User, username=username)
+    post_list = Post.objects.filter(author=author).all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    post_quantity = post_list.count()
     context = {
-        'user': user,
+        'author': author,
         'page_obj': page_obj,
         'post_quantity': post_quantity,
     }
@@ -71,19 +68,19 @@ def post_create(request):
     """The view creates a new post by a special form."""
     form = PostForm()
     groups = Group.objects.all()
-    context = {
-        'form': form,
-        'groups': groups,
-    }
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             new_post = form.save(False)
             new_post.author = request.user
             new_post.save()
-            return redirect('posts:profile', username=request.user)
-    else:
-        form = PostForm()
+            return redirect('posts:profile', username=request.user.username)
+        else:
+            form = PostForm()
+    context = {
+        'form': form,
+        'groups': groups,
+    }
     return render(request, 'posts/create_post.html', context)
 
 
@@ -95,24 +92,22 @@ def post_edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     form = PostForm(instance=post)
     groups = Group.objects.all()
+    author = post.author.username
     is_edit = True
     context = {
         'form': form,
         'is_edit': is_edit,
         'groups': groups,
     }
+    if author != request.user:
+        return redirect('posts:post_detail', pk=post_id)
+
     if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if post.author != request.user:
-            return redirect('posts:post_detail', pk=post_id)
-        elif post.author == request.user:
+        form = PostForm(request.POST, instance-post)
+        if request.user == author:
             if form.is_valid():
-                post.author = request.user
-                post.pk = post_id
-                post.text = form.cleaned_data['text']
-                post.save()
+                post.form.save()
                 return redirect('posts:post_detail', pk=post_id)
         else:
-            PostForm(instance=post)
-            return redirect('posts/create_post.html')
+            return redirect('posts:post_detail', pk=post_id)
     return render(request, 'posts/create_post.html', context)
